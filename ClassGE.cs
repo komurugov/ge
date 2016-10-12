ffb5ad93cc03ae8b34dd3b53207b0f756481e766
Konstantin Murugov <komurugov@gmail.com>
2016-10-12 14:44:56 +0300

    Missed files are added.

---
+++ ClassGE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace geApp
+{
+    class ClassGE
+    {
+        List<ClassShape> _shapes = new List<ClassShape>();
+        public void AddShape(Type t)
+        {
+            if (t == typeof(ClassShapeEllipse))
+                _shapes.Add(new ClassShapeEllipse());
+            else if (t == typeof(ClassShapePolygon))
+                _shapes.Add(new ClassShapePolygon());
+        }
+    }
+}
